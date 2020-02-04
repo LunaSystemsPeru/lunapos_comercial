d4aa131f9f29e63fa08471cbf78d69d73e53ffe9
Luis Oyanguren <leog.1992@gmail.com>
2020-02-04 14:46:07 -0500

    eliminar proveedor

--- src/clases/cl_ingresos.java
+++ src/clases/cl_ingresos.java
@@ -321,5 +321,18 @@ public class cl_ingresos {
         c_conectar.cerrar(st);
         return registrado;
     }
+    
+    public boolean eliminar_proveedor() {
+        boolean registrado = false;
+        Statement st = c_conectar.conexion();
+        String query = "delete from ingresos "
+                + "where id_proveedor = '" + id_proveedor + "'";
+        int resultado = c_conectar.actualiza(st, query);
+        if (resultado > -1) {
+            registrado = true;
+        }
+        c_conectar.cerrar(st);
+        return registrado;
+    }
 
 }
--- src/clases/cl_proveedor.java
+++ src/clases/cl_proveedor.java
@@ -141,6 +141,20 @@ public class cl_proveedor {
         c_conectar.cerrar(st);
         return registrado;
     }
+    
+    public boolean eliminar() {
+        boolean registrado = false;
+        Statement st = c_conectar.conexion();
+        String query = "delete from proveedor "
+                + "where id_proveedor= '" + id_proveedor + "'";
+        System.out.println(query);
+        int resultado = c_conectar.actualiza(st, query);
+        if (resultado > -1) {
+            registrado = true;
+        }
+        c_conectar.cerrar(st);
+        return registrado;
+    }
 
     public void obtener_codigo() {
         try {
